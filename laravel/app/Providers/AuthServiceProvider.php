ea6ce35d2fd5ee3bd380c4a25b16d2cf74ea9485
saboyutaka <yutaka.paradigm.shift@gmail.com>
2019-11-09 14:49:16 +0900

    :+1: Laravel Passport でOAuth 2.0を実装

--- laravel/app/Providers/AuthServiceProvider.php
+++ laravel/app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Laravel\Passport\Passport;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,6 +26,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Passport::routes(
+            function ($router) {
+                $router->forAuthorization();
+                $router->forAccessTokens();
+                $router->forTransientTokens();
+                // $router->forClients();
+                // $router->forPersonalAccessTokens();
+            }
+        );
     }
 }
