a26301189b9e762a80565b60818e67dd18b5fa0f
saboyutaka <yutaka.paradigm.shift@gmail.com>
2019-11-09 15:25:11 +0900

    :+1: nuxt用のpassport clientを作成

--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        Laravel\Passport\Client::create([
+            'id' => env('LARAVEL_PASSPORT_CLIENT_ID'),
+            'name' => 'nuxt-auth',
+            'secret' => env('LARAVEL_PASSPORT_CLIENT_SECRET'),
+            'redirect' => env('NUXT_CALLBACK_URL'),
+            'personal_access_client' => 0,
+            'password_client' => 0,
+            'revoked' => 0,
+        ]);
     }
 }
