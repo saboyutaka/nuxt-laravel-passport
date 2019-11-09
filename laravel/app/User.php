a10bd8130593292afbb88272c2abc711b349d614
saboyutaka <yutaka.paradigm.shift@gmail.com>
2019-11-09 15:16:30 +0900

    :+1: apiの認証をpassportに変更

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -3,12 +3,13 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
